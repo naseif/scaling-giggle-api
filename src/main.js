0822fbbc88bad146155122a762df867e95bf121e
naseif <d2mds21@gmail.com>
2021-06-06 16:04:31 +0000

    Created first test files. and tested a little bit

---
+++ src/main.js
@@ -0,0 +1,32 @@
+const express = require('express')
+let app = express()
+const port = 51337;
+const cors = require("cors")
+const fs = require("fs");
+const path = __dirname
+
+const questionFilesFilter = fs.readdirSync(path).filter((file) => file.endsWith(".json"));
+let questionFilesWithPath = [] 
+for (const file of questionFilesFilter) {
+    const files = require(`${path}/${file}`)
+    questionFilesWithPath.push(files)
+} 
+
+
+app.use(cors())
+
+app.get(`/training/:trainingID/question/:questionID`, function (req, res) {
+
+    const traininglang = questionFilesWithPath.filter((file) => file.trainingID.includes(req.params.trainingID))
+    
+    try {
+        res.json(traininglang[0].questions[req.params.questionID]);
+    } catch (err) {
+        res.status(500).send('Something broke!');
+    } 
+
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})

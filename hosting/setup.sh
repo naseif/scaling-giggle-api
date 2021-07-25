4d7c1933368c1a15c30e476bceecf3775a3b0ee6
Stefan Hoffmann <stho32github@gmail.com>
2021-07-25 18:29:52 +0200

    changed the way the certs are generated

--- hosting/setup.sh
+++ hosting/setup.sh
@@ -21,10 +21,7 @@ npm i
 cd hosting
 
 ## generate self signed ssl certificate
-openssl genrsa -out key.pem
-openssl req -new -key key.pem -out csr.pem
-openssl x509 -req -days 9999 -in csr.pem -signkey key.pem -out cert.pem
-rm csr.pem
+openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365
 
 ## daemon stuff
 systemctl daemon-reload

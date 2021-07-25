34428e4b2b72f07b9b1fbc56715138a3118ad737
Stefan Hoffmann <stho32github@gmail.com>
2021-07-25 17:24:47 +0200

    setup.sh + npm i

--- hosting/setup.sh
+++ hosting/setup.sh
@@ -15,6 +15,10 @@ sudo apt-get install -y nodejs
 ## install the service
 cp scaling-giggle-env.service /lib/systemd/system/scaling-giggle-env.service
 
+## we need to install the npm packages ... 
+cd ..
+npm i
+
 systemctl daemon-reload
 systemctl start scaling-giggle-env.service
 systemctl enable scaling-giggle-env.service
